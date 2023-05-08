e9e741392b769bb5f36bf96035b1df481fadcfd0
Dhon23 <m.ramadhan.ak@gmail.com>
2023-05-08 21:58:31 +0700

    merge same image name to pdf

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
-import fs from "fs"
+import fs from "fs";
 import PDFMerger from "pdf-merger-js";
+import imgToPdf from "image-to-pdf";
+import Jimp from "jimp";
 
 const generateRandomStr = () => {
   const length = 16;
@@ -17,46 +19,77 @@ const generateRandomStr = () => {
   return password;
 };
 
+function fileSizeMegaBytes(filename) {
+  const stats = fs.statSync(filename);
+  const fileSizeInBytes = stats.size;
+  return fileSizeInBytes / (1024 * 1024);
+}
+
 (async () => {
+  fs.readdir("output", null, (err, files) => {
+    files.forEach((el) => fs.unlinkSync("output" + "/" + el));
+  });
   const files = fs.readdirSync("input");
 
   const groups = {};
-  const copies = [];
 
   files.forEach((file) => {
-    const match = /^(.*?)_/.exec(file);
-    if (match && file.match(/(.pdf)$/)) {
-      const key = match[1];
+    const match = /^(.*?)(?:_| )/.exec(file);
+    const ext = file.match(/\.(\w+)$/)[1];
+    if (match) {
+      const key = match[1] + "." + ext;
       if (!groups[key]) {
         groups[key] = [];
       }
       groups[key].push(file);
-    } else if (!match && file.match(/(.pdf)$/)) {
-      copies.push(file);
-    } else if (file.match(/(.(jpg|jpeg|png))$/)) {
-      copies.push(file);
     }
   });
 
   for (const key in groups) {
-    const merger = new PDFMerger();
-    const randomStr = generateRandomStr();
-    for (const fileName of groups[key]) {
-      if (fileName.match(/(.pdf)$/)) {
-        const fileBuffer = fs.readFileSync("input/" + fileName);
-        await merger.add(fileBuffer);
+    const randomStr = "-" + generateRandomStr();
+    const nameWithoutExt = key.replace(/\.[^.]+$/, "");
+    if (key.match(/pdf$/)) {
+      const merger = new PDFMerger();
+      for (const fileName of groups[key]) {
+        if (fileName.match(/(.pdf)$/)) {
+          const fileBuffer = fs.readFileSync("input/" + fileName);
+          await merger.add(fileBuffer);
+        }
       }
-    }
-    await merger.save("output/" + key + "-" + randomStr + ".pdf");
-  }
+      await merger.save("output/" + nameWithoutExt + randomStr + ".pdf");
+    } else if (groups[key].length > 1) {
+      const withParent = await Promise.all(
+        groups[key].map(async (el) => {
+          let filePath = "input/" + el;
+          if (fileSizeMegaBytes(filePath) > 0.8) {
+            const out = "temp/" + el;
+            const image = await Jimp.read(filePath);
+            image.resize(1000, Jimp.AUTO).quality(85);
+            await image.writeAsync(out);
+            filePath = out;
+          }
+          return filePath;
+        })
+      );
 
-  for (const copy of copies) {
-    const randomStr = generateRandomStr();
-    const nameWithoutExt = copy.replace(/\.[^.]+$/, "");
-    const ext = "." + copy.match(/\.([^.]+)$/)[1];
-    fs.copyFileSync(
-      "input/" + copy,
-      "output/" + nameWithoutExt + "-" + randomStr + ext
-    );
+      imgToPdf(withParent, imgToPdf.sizes.A4).pipe(
+        fs.createWriteStream(
+          "output" + "/" + nameWithoutExt + randomStr + ".pdf"
+        )
+      );
+
+      fs.readdir("temp", null, (err, files) => {
+        files.forEach((el) => fs.unlinkSync("temp" + "/" + el));
+      });
+
+    } else {
+      const filename = groups[key][0];
+      const withoutExt = filename.replace(/\.[^.]+$/, "");
+      const ext = "." + filename.match(/\.([^.]+)$/)[1];
+      fs.copyFileSync(
+        "input/" + filename,
+        "output/" + withoutExt + randomStr + ext
+      );
+    }
   }
 })();
--- upload.js
+++ upload.js
@@ -1,23 +1,43 @@
-const fs = require("fs");
+import fs from "fs";
+
+const name = process.argv[2];
 
 (async () => {
   const output = fs.readdirSync("output");
+
+  const relativePaths = [];
 
   for (const fileName of output) {
     const arraybuffer = fs.readFileSync("output/" + fileName);
     const toBlob = new Blob([arraybuffer]);
     const fd = new FormData();
-    fd.append("directory", "cms_dev/document-progress");
+    fd.append("directory", "cms/document-progress/" + name);
+    // fd.append("directory", "cms_dev/document-progress");
     fd.append("origin", "public_uploads");
     fd.append("file", toBlob, fileName);
     //"https://fm.prod.marketa.id/media-manager/upload"
     //"https://fm.superwa.io/media-manager/upload"
-    const response = await fetch("https://fm.superwa.io/media-manager/upload", {
-      method: "POST",
-      body: fd,
-    })
+    const response = await fetch(
+      "https://fm.prod.marketa.id/media-manager/upload",
+      {
+        method: "POST",
+        body: fd,
+      }
+    )
       .then((res) => res.json())
-      .then(({ data }) => data);
-    console.log(response.uploadRelativePath);
+      .then(({ data }) => data)
+      .catch((err) => err);
+
+    const { uploadRelativePath } = response || {};
+
+    if (uploadRelativePath) relativePaths.push(uploadRelativePath);
+    console.log(response?.uploadRelativePath || response);
   }
+
+  if (!fs.existsSync("uploads.json")) fs.appendFileSync("uploads.json", "{}");
+  fs.readFile("uploads.json", (err, data) => {
+    const json = JSON.parse(data);
+    json[name] = relativePaths;
+    fs.writeFileSync("uploads.json", JSON.stringify(json, null, 2));
+  });
 })();
